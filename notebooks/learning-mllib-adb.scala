20e6d13a51cfbdd6751edf1d0ce29aee757b66f5
Dayashankar Kushwaha <32188252+daya-shankar07@users.noreply.github.com>
2019-05-30 12:02:16 +0530

    Update: README.md notebooks

---
+++ notebooks/learning-mllib-adb.scala
@@ -0,0 +1,9 @@
+// Databricks notebook source
+
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.{LabeledPoint, LinearRegressionWithSGD}
+
+
+
+// COMMAND ----------
+
